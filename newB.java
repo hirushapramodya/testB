dcf629f8f01da3b217a7be5d2324f0cf129c7c2d
Your Name <your.email@example.com>
2024-08-01 19:38:57 +0530

    create a another file

---
+++ newB.java
@@ -0,0 +1,16 @@
+public class newB {
+    
+        void call(){
+          System.out.println("take a call");
+    
+        }
+        void run()
+        {
+            System.out.println("running");
+        }
+        public static void main(String[] args) {
+            newB ob = new newB();
+            ob.call();
+        }
+    }
+
