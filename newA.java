3e20086939685676679f8a0b95c52f9c9e7ba403
Your Name <your.email@example.com>
2024-08-01 19:14:35 +0530

    create a new method file

---
+++ newA.java
@@ -0,0 +1,10 @@
+public class newA {
+    void call(){
+      System.out.println("take a call");
+
+    }
+    public static void main(String[] args) {
+        newA ob = new newA();
+        ob.call();
+    }
+}
